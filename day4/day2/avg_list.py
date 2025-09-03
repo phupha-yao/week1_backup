0148fdd153dbde621ee79a432633cde71ef28056
pp <pp@phupha.local>
2025-09-03 22:47:57 +0700

    Remove folder_name

--- day4/day2/avg_list.py
+++
@@ -1,8 +0,0 @@
-print("Please input a list of numbers, separated by spaces:")
-user_input = input()
-numbers = user_input.split()
-average = 0
-for x in range(0, len(numbers)):
-    average = average + float(numbers[x])
-average = average / len(numbers)
-print("The average is:", average)
--- day4/day3/max_in_list.py
+++
@@ -1,9 +0,0 @@
-def max_in_list(n: list):
-    max = n[0]
-    for i in range(1, len(n)):
-        if n[i] > max:
-            max = n[i]
-    return max
-
-if __name__ == "__main__":
-    print(max_in_list([-1, -5, -4]))
